57a745d30b23c663711b22540ff4895365d7b005
Rohan Bhutani <rohanbhutani1993@gmail.com>
2022-05-22 16:11:33 +0100

    Adding ExpenseItem.css

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import ExpenseItem from "./components/ExpenseItem";
 function App() {
   return (
     <div>
-      <h1>Let's get started</h1>
       <ExpenseItem />
     </div>
   );
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,5 +1,15 @@
+import "./ExpenseItem.css";
+
 function ExpenseItem() {
-  return <h2>This is an expense item.</h2>;
+  return (
+    <div className="expense-item">
+      <div>Date</div>
+      <div className="expense-item__description">
+        <h2>Car Insurance</h2>
+        <div className="expense-item__price">$2980</div>
+      </div>
+    </div>
+  );
 }
 
 export default ExpenseItem;
